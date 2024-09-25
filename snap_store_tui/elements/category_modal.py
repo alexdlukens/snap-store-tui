df38a34b2a014898ed6b581750b762a0a81f3f7f
Alex Lukens <alex.lukens@canonical.com>
2024-09-25 04:34:13 -0400

    add some css to categories file

--- snap_store_tui/elements/category_modal.py
+++ snap_store_tui/elements/category_modal.py
@@ -1,18 +1,27 @@
+from pathlib import Path
+
 from textual.app import ComposeResult
 from textual.screen import ModalScreen
 from textual.widgets import OptionList
 
+CATEGORY_CSS_PATH = Path(__file__).parent.parent / "styles" / "category_modal.tcss"
+
 
 class CategoryModal(ModalScreen):
-    def __init__(self, categories: list[tuple[str, str]]) -> None:
+    CSS_PATH = CATEGORY_CSS_PATH
+
+    def __init__(
+        self, categories: list[tuple[str, str]], current_category: str
+    ) -> None:
         super().__init__()
         self.categories = categories
+        self.current_category = current_category
 
     def compose(self) -> ComposeResult:
-        yield OptionList(
-            *self.categories,
-            name="category",
+        option_list = OptionList(
+            *self.categories, name="category", id="category_option_list", wrap=False
         )
+        yield option_list
 
     def on_option_list_option_selected(self, option: OptionList.OptionSelected) -> None:
         self.dismiss(option.option.prompt)
