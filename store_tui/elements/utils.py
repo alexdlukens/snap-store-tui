006e2f97791696effe6855c05bc5121297305467
Alexander Lukens <alexdlukens@gmail.com>
2024-11-18 21:32:08 -0500

    add docstring for get_platform_architecture method

--- store_tui/elements/utils.py
+++ store_tui/elements/utils.py
@@ -1,12 +1,17 @@
 import platform
 
 
-def get_platform_architecture():
+def get_platform_architecture() -> str:
+    """Use platform module to get the machine architecture and remap to snapcraft expectations for architectures
+
+    Returns:
+        str: current system architecture
+    """
     machine_arch = platform.machine()
 
     # remap to snap architectures
     if machine_arch == "x86_64":
         return "amd64"
-    elif machine_arch == "aarch64":
+    if machine_arch == "aarch64":
         return "arm64"
     return machine_arch
