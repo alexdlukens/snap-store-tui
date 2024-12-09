3a52a54a11d3b609721702b1f061a1abd82f813e
Alexander Lukens <alexdlukens@gmail.com>
2024-12-08 21:41:01 -0500

    use leaf nodes on nodes that should not be expandable

--- store_tui/elements/snap_channel_tree.py
+++ store_tui/elements/snap_channel_tree.py
@@ -18,16 +18,16 @@ class SnapChannelTree(Widget):
         self.channel_tree = Tree(channel_name, classes="snap-channel-info")
         self.channel_tree.styles.overflow_x = "hidden"
         self.channel_tree.root.expand()
-        self.channel_tree.root.add(f"Revision: {channel.revision}")
-        self.channel_tree.root.add(
+        self.channel_tree.root.add_leaf(f"Revision: {channel.revision}")
+        self.channel_tree.root.add_leaf(
             f"Size: {humanize.naturalsize(channel.download.size)}"
         )
-        self.channel_tree.root.add(f"Version: {channel.version}")
+        self.channel_tree.root.add_leaf(f"Version: {channel.version}")
         release_node = self.channel_tree.root.add(
             f"Released: {humanize.naturaltime(channel.channel.released_at)}"
         )
         release_node.add(f"{channel.channel.released_at}")
-        self.channel_tree.root.add(f"Confinement: {channel.confinement}")
+        self.channel_tree.root.add_leaf(f"Confinement: {channel.confinement}")
 
         if self.is_mounted:
             self.refresh(recompose=True)
